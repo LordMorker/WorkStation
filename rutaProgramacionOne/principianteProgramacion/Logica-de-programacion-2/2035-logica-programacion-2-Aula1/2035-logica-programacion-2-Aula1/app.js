9d13c3d6c1f3b81e74c5a08c9bbc13fb3592fb98
Andres <lordmorker@gmail.com>
2024-06-24 14:46:12 -0500

    modificando y añadiendo archivos

---
+++ rutaProgramacionOne/principianteProgramacion/Logica-de-programacion-2/2035-logica-programacion-2-Aula1/2035-logica-programacion-2-Aula1/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector(`h1`);
+titulo.innerHTML = 'Juego del numero secreto';
+
+let párrafo = document.querySelector(`p`);
+párrafo.innerHTML = 'elige un numero del 1 al 10';
+
+function intentoDeUsuario() {
+    alert('click desde el botón');
+};
