bee39f18a82082999d4d92a8e5a56d3bf07363e8
Andres <lordmorker@gmail.com>
2024-06-27 02:02:14 -0500

    organizando archivos

--- rutaProgramacionOne/formacionOne/logica de programacion/logica-de-programacion-1/2034-logica-programacion-1-Aula3/2034-logica-programacion-1-Aula3/prueba.js
+++
@@ -1,7 +0,0 @@
-//variables reto 3 
-let numUser = prompt('digite un numero');
-//este bloque genera un bucle para contar de numUser a 0
-while (numUser >= 0) {
-    console.log(numUser);
-    numUser--;
-}
